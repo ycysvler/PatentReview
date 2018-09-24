3119bf4a2d006516a8726fa95b6c7c68ebdb3a43
zhanghongqing@interrcs.com <zhanghongqing@interrcs.com>
2018-09-24 18:14:17 +0800

    * add api heartbeat

--- server/routers/api/heartbeat.js
+++ server/routers/api/heartbeat.js
@@ -4,23 +4,12 @@
  * Created by VLER on 2018/7/18.
  */
 const moment = require('moment');
-const HaMasterLogic = require('../../logic/hamasterlogic');
 
 module.exports = function (router) {
     router.get('/heartbeat', async (ctx) => {
         ctx.body = {
             code: 200,
-            date: moment().format("YYYY-MM-DD hh:mm:ss")
+            date: moment().format("YYYY-MM-DD HH:mm:ss")
         };
-    });
-
-    router.post('/heartbeat', async(ctx)=>{
-        let logic = new HaMasterLogic();
-        let body = ctx.request.body;
-        body.updatetime = moment();
-
-        let item = await logic.heartbeat(body.orgid, body.type, body);
-
-        ctx.body = {code:200, data:item};
     });
 };
