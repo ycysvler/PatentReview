074229e0e51a151be65710fb4602f5b9c88471aa
zhanghongqing@interrcs.com <zhanghongqing@interrcs.com>
2018-09-25 18:21:44 +0800

    * 我勒个去，必须指定webpack,webpack-cli版本，不然编译不通过；

--- app/main.js
+++ app/main.js
@@ -1,23 +1,21 @@
-import "whatwg-fetch";
-import "promise-polyfill/src/polyfill";
+// import "whatwg-fetch";
+// import "promise-polyfill/src/polyfill";
 import React from "react";
 import {render} from "react-dom";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 import {applyMiddleware, createStore} from "redux";
 import {Route, Router, Switch} from "react-router-dom";
-import BrowserHistory from "./history/browserhistory";
+// import BrowserHistory from "./history/browserhistory";
 import reducer from "./reducers";
 import App from "./app";
-import Common from "./common";
+// import Common from "./common";
 
-Common.checkVer();
 const store = createStore(reducer, applyMiddleware(thunk));
 const router = (
     <Provider store={store}>
-        <Router history={BrowserHistory}>
+        <Router >
             <Switch>
-                <Route exact path="/signin" component={App}/>
                 <Route strict path="/" component={App}/>
             </Switch>
         </Router>
